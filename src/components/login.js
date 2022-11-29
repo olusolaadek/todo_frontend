b506b092439c19de2bb145d1e1201f9594766e60
Olusola Adekunle <olusolaadek@hotmail.com>
2022-11-29 21:27:16 +0100

    implement TodoService with axios

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
-const Login = () => {
+const Login = (props) => {
+    const [username, setUsername] = useState("")
+    const [password, getPassword] = useState("")
+
+    const onChangeUsername = (e) => {
+        const username = e.target.value;
+        setUsername(username);
+    }
+
+    const onChnagePassword = (e) => {
+        password = e.target.value;
+        setPassword(password);
+    }
+
     return (
         <div>
             <h1>Login</h1>
